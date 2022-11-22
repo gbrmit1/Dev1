4afb434df54809ccf8a5b200fbedde0f0dbbde09
gbrmit1 <robert.mitchell@atradius.com>
2022-11-22 12:09:54 +0000

    updated ps1 file again

--- 01.server.ps1
+++ 01.server.ps1
@@ -5,5 +5,9 @@ New-AzResourceGroup -Name $rg -Location northeurope
 New-AzResourceGroupDeployment `
 -Name RM-Dev `
 -ResourceGroupName RM-ARM-RG-01 `
--TemplateFile /home/mitchell/01.server.json `
--TemplateParameterFile /home/mitchell/01.server.parameters.json
+-TemplateUri https://raw.githubusercontent.com/gbrmit1/Dev1/main/01.server.json `
+-TemplateParameterUri https://raw.githubusercontent.com/gbrmit1/Dev1/main/01.server.parameters.json
+
+Write-Host "
+Finished Deployment Script!
+"
