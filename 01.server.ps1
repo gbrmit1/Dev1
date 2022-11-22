309ad87dba9f4cda08c9b98e1ad4eb5c4be888a6
gbrmit1 <robert.mitchell@atradius.com>
2022-11-22 09:33:58 +0000

    updated ps1 file

--- 01.server.ps1
+++ 01.server.ps1
@@ -2,4 +2,8 @@ $rg = 'RM-ARM-RG-01'
 
 New-AzResourceGroup -Name $rg -Location northeurope
 
-New-AzResourceGroupDeployment -Name RM-Dev -ResourceGroupName RM-ARM-RG-01 -TemplateFile /home/mitchell/01.server.json -TemplateParameterFile /home/mitchell/01.server.parameters.json
+New-AzResourceGroupDeployment `
+-Name RM-Dev `
+-ResourceGroupName RM-ARM-RG-01 `
+-TemplateFile /home/mitchell/01.server.json `
+-TemplateParameterFile /home/mitchell/01.server.parameters.json
